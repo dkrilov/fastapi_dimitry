932bc24cc87a1805d8780407e9591228137feba1
dkrilov <dkrilov@factset.com>
2022-08-08 15:34:23 +0900

    Updated main and requirements files

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+# domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
 @app.get("/")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"message": "Hello Dimitry!"}
